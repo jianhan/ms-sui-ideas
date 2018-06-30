b8aa05a6214c9f271069c3722fcc9e02dc680dc9
Jian Han <jianhan@mail.com>
2018-06-30 20:06:49 +1000

    [DEV] Added story handler, updated main

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jianhan/ms-sui-ideas/middleware"
 	"github.com/jianhan/ms-sui-ideas/mongodb"
 	"github.com/jianhan/ms-sui-ideas/proto/occupation"
+	"github.com/jianhan/ms-sui-ideas/proto/story"
 	cfgreader "github.com/jianhan/pkg/configs"
 	"github.com/micro/go-micro"
 	"github.com/nats-io/go-nats-streaming"
@@ -50,6 +51,11 @@ func main() {
 	occupation.RegisterOccupationServiceHandler(
 		srv.Server(),
 		handler.NewOccupation(mongodb.Occupation(session, viper.GetString("mongodb.db"), "occupations"), sc),
+	)
+
+	story.RegisterStoryServiceHandler(
+		srv.Server(),
+		handler.NewStory(mongodb.Story(session, viper.GetString("mongodb.db"), "stories"), sc),
 	)
 
 	// init service
